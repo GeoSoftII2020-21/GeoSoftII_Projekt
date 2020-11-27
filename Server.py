eef8f8f2401f296610b498def82b9bd428bd93af
Leg-en <44808950+Leg-en@users.noreply.github.com>
2020-11-27 22:32:42 +0100

    Comments Edited

--- Server.py
+++ Server.py
@@ -1,123 +1,148 @@
-from flask import Flask, request, jsonify, json
-
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-#Get Request für die Kurzübersicht über die API
 @app.route("/", methods=['GET'])
 def default():
     """
     Soll ein JSON mit der Kurzübersicht unserer API Returnen
     :return: JSON
     """
-    # Entnommen aus der OpenEO API
-    #Todo: Anpassen
-    #API Übersicht
-    data = { "api_version": "1.0.0", "backend_version": "1.1.2", "stac_version": "string", "id": "cool-eo-cloud", "title": "WWU Geosoft2 Projekt", "description": "WWU Projekt", "production": False, "endpoints": [ { "path": "/collections", "methods": [ "GET" ] }, { "path": "/processes", "methods": [ "GET" ] }, { "path": "/jobs", "methods": [ "GET", "POST" ] }, { "path": "/jobs/{job_id}", "methods": [ "DELETE", "PATCH" ] }, { "path": "/jobs/{job_id}/result", "methods": [ "GET", "POST" ] } ], "links": [ { "href": "https://www.uni-muenster.de/de/", "rel": "about", "type": "text/html", "title": "Homepage of the service provider" } ] }
+    # Todo: Anpassen
+    data = {"api_version": "1.0.0", "backend_version": "1.1.2", "stac_version": "string", "id": "cool-eo-cloud",
+            "title": "WWU Geosoft2 Projekt", "description": "WWU Projekt", "production": False,
+            "endpoints": [{"path": "/collections", "methods": ["GET"]}, {"path": "/processes", "methods": ["GET"]},
+                          {"path": "/jobs", "methods": ["GET", "POST"]},
+                          {"path": "/jobs/{job_id}", "methods": ["DELETE", "PATCH"]},
+                          {"path": "/jobs/{job_id}/result", "methods": ["GET", "POST"]}], "links": [
+            {"href": "https://www.uni-muenster.de/de/", "rel": "about", "type": "text/html",
+             "title": "Homepage of the service provider"}]}
     return jsonify(data)
 
-#Gibt die Collections aus bei Get Request
+
 @app.route("/collections", methods=['GET'])
 def collections():
     """
-    Return alle vorhandenen Collections bei einer GET Request
-    :rtype: object
+    Returnt alle vorhandenen Collections bei einer GET Request
+
+    :returns:
+        jsonify(data): Alle Collections in einer JSON
     """
-    #Docstring
+    # Docstring
 
-    #Collections müssten Abgefragt werden
+    # Collections müssten Abgefragt werden
     data = None
-    return jsonify(collections)
+    return jsonify(data)
 
-#Gibt die Processes aus bei Get Request
+
+# Gibt die Processes aus bei Get Request
 @app.route("/processes", methods=['GET'])
 def processes():
     """
-    Return die Processes
-    :return: Processes
+    Returnt alle vorhandenen processes bei einer GET Request
+    :returns:
+        jsonify(data): Alle processes in einer JSON
     """
-    #Processes müssten Abgefragt werden
+    # Processes müssten Abgefragt werden
     data = None
-    return jsonify(processes)
+    return jsonify(data)
 
-#Get Reuqest für Jobs
+
+# Get Reuqest für Jobs
 @app.route("/jobs", methods=['GET'])
 def jobsGET():
     """
-    Return die Jobs
-    :return: Jobs
+    Returnt alle vorhandenen jobs bei einer GET Request
+    :returns:
+        jsonify(data): Alle jobs in einer JSON
     """
     data = None
     return jsonify(data)
 
-#Post Request für Jobs
+
 @app.route("/jobs", methods=['POST'])
 def jobsPOST():
     """
     Nimmt den Body eines /jobs post request entgegen
-    :return:
+    :returns:
+        jsonify(data): HTTP Statuscode für Erfolg (?)
     """
     dataFromPost = request.get_json()
     data = None
     return jsonify(data)
 
-#Patch  Request. Id ist erstmal als int angegeben. Andere Datentypen wie String UUID auch nutzbar
-@app.route("/patch/<int:id>",methods=['PATCH'])
+
+@app.route("/patch/<int:id>", methods=['PATCH'])
 def patchFromID(id):
     """
-    Nimmt den Body eine Patch request mit einer ID entgegen
-    :param id:
-    :return:
+    Nimmt den Body einer Patch request mit einer ID entgegen
+    :parameter:
+        id (int): Nimmt die ID aus der URL entgegen
+    :returns:
+        jsonify(data): HTTP Statuscode für Erfolg (?)
     """
     dataFromPatch = request.get_json()
     data = None
     return jsonify(data)
 
-#Nimmt eine Delete Request entgegen
-@app.route("/patch/<int:id>",methods=["DELETE"])
+
+@app.route("/patch/<int:id>", methods=["DELETE"])
 def deleteFromID(id):
     """
-    Nimmt eine Delete Request entgegen
-    :param id:
-    :return:
+    Nimmt eine Delete request für eine ID Entgegen
+    :parameter:
+        id (int): Nimmt die ID aus der URL entgegen
+    :returns:
+        jsonify(data): HTTP Statuscode für Erfolg (?)
     """
     data = None
     return jsonify(data)
 
-#Post methode zum Job Starten. Nimmt JSON Body entgegen und erwartet ID
-@app.route("/jobs/<int:id>/results",methods=['POST'])
+
+@app.route("/jobs/<int:id>/results", methods=['POST'])
 def startFromID(id):
     """
-    Post methode zum Job Starten. Nimmt JSON Body entgegen und erwartet ID
-    :param id:
-    :return:
+    Startet einen Job aufgrundlage einer ID aus der URL. Nimmt ebenso den Body der Post request entgegen
+    :parameter:
+        id (int): Nimmt die ID aus der URL entgegen
+    :returns:
+        jsonify(data): HTTP Statuscode für Erfolg (?)
     """
     dataFromPost = request.get_json()
     data = None
     return jsonify(data)
 
-#Gibt die Ergebnisse eines Jobs aus
-@app.route("/jobs/<int:id>/results",methods=["GET"])
+
+@app.route("/jobs/<int:id>/results", methods=["GET"])
 def getJobFromID(id):
     """
-    Gibt die Ergebnisse eines Jobs aus
-    :param id:
-    :return:
+    Nimmt den Body eine Patch request mit einer ID entgegen
+    :parameter:
+        id (int): Nimmt die ID aus der URL entgegen
+    :returns:
+        jsonify(data): Ergebnis des Jobs welcher mit der ID assoziiert ist.
     """
     data = None
     return jsonify(data)
 
-#Lässt dateien Upload zu
-@app.route("/data",methods=["POST"])
+
+@app.route("/data", methods=["POST"])
 def postData():
     """
-    Lässt dateien Upload zu
-    :return:
+    Custom Route, welche nicht in der OpenEO API Vorgesehen ist. Nimmt die daten der Post request entgegen.
+    :returns:
+        jsonify(data): HTTP Statuscode für Erfolg (?)
     """
     dataFromPost = request.get_json()
     data = None
-    return data
+    return jsonify(data)
 
-# Startet den Job
-if __name__ == "__main__":
+
+def main():
+    """
+    Startet den Server. Aktuell im Debug Modus und Reagiert auf alle eingehenden Anfragen auf Port 80.
+    """
     app.run(debug=True, host="0.0.0.0", port=80)
+
+
+if __name__ == "__main__":
+    main()
