f7f2c2c267b27c415a4e0b06ec2269f814d5ed0f
Sterthaus <44808950+Leg-en@users.noreply.github.com>
2021-01-19 17:41:11 +0100

    ID Sicherheit angepasst

---
+++ dockerBuildAndPush.cmd
@@ -0,0 +1,20 @@
+cd ..
+cd GeoSoftII_DataServer
+docker build -t felixgi1516/geosoft2_dataserver .
+docker push felixgi1516/geosoft2_dataserver
+cd ..
+cd GeoSoftII_Frontend
+docker build -t felixgi1516/geosoft2_frontend .
+docker push felixgi1516/geosoft2_frontend
+cd ..
+cd GeoSoftII_JobManagement
+docker build -t felixgi1516/geosoft2_jobmanagement .
+docker push felixgi1516/geosoft2_jobmanagement
+cd ..
+cd GeoSoftII_NDVI_Process
+docker build -t felixgi1516/geosoft2_ndvi_process .
+docker push felixgi1516/geosoft2_ndvi_process
+cd ..
+cd GeoSoftII_SST_Process
+docker build -t felixgi1516/geosoft2_sst_process .
+docker push felixgi1516/geosoft2_sst_process
