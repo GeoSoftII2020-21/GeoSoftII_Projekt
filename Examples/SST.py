6903ea19346fad8eb9695f83e22f145da575092e
Sterthaus <44808950+Leg-en@users.noreply.github.com>
2021-02-06 17:15:29 +0100

    Processing server awaits now the database, Comments added
    Co-authored-by: Svub1 <s_buse01@uni-muenster.de>

--- Examples/SST.py
+++ Examples/SST.py
@@ -5,9 +5,9 @@ print("Öffne File..")
 with open("SST.json") as json_file:
     data = json.load(json_file) #Lade den JSON job ein
 print("Sende Post request..")
-r = requests.post("http://localhost:8080/api/v1/jobs", json=data) #Sende Job an den Server
+r = requests.post("http://localhost:80/api/v1/jobs", json=data) #Sende Job an den Server
 ident = r.headers["OpenEO-Identifier"] #Kopiere Identifier
 print("ID: " + ident)
 print("Starte Job..")
-url = "http://localhost:8080/api/v1/jobs/"+ str(ident) +"/results" #Baue URL
+url = "http://localhost:80/api/v1/jobs/"+ str(ident) +"/results" #Baue URL
 req = requests.post(url) #Starte Job
